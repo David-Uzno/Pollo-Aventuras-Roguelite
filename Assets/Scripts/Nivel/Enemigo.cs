480b96396779632a60d73571270f6c11ad339faa
MateoMonchi <monchiettimateo1@gmail.com>
2024-05-11 00:27:54 -0300

    Feat:Daño a los enemigos

--- Assets/Scripts/Nivel/Enemigo.cs
+++ Assets/Scripts/Nivel/Enemigo.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
+    [SerializeField] private float vida;
     [SerializeField] private Transform _seguirJugador;
     [SerializeField] private float _velocidadMovimiento = 5f;
+    [SerializeField] private Vector2 _limitesMovimientoMin = new Vector2(-5f, -5f); // Límites mínimos de movimiento
+    [SerializeField] private Vector2 _limitesMovimientoMax = new Vector2(5f, 5f); // Límites máximos de movimiento
 
     private void Update()
     {
@@ -16,16 +19,30 @@ public class Enemigo : MonoBehaviour
             direccion.Normalize(); // Normalizar para obtener la dirección correcta
 
             // Mover el enemigo hacia el jugador
-            transform.Translate(direccion * _velocidadMovimiento * Time.deltaTime);
+            Vector3 newPosition = transform.position + direccion * _velocidadMovimiento * Time.deltaTime;
+
+            // Aplicar límites de movimiento
+            newPosition.x = Mathf.Clamp(newPosition.x, _limitesMovimientoMin.x, _limitesMovimientoMax.x);
+            newPosition.y = Mathf.Clamp(newPosition.y, _limitesMovimientoMin.y, _limitesMovimientoMax.y);
+
+            transform.position = newPosition;
 
             // Rotar el enemigo hacia la dirección del jugador (opcional)
             transform.rotation = Quaternion.LookRotation(Vector3.forward, direccion);
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D otro)
+    public void TomarDaño(float daño)
     {
-        if (otro.gameObject.CompareTag("Jugador"))
+        vida -= daño;
+        if (vida <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Jugador"))
         {
             GameManager.Instance.PerderVida();
         }
